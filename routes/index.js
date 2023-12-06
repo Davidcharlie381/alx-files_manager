41719cfd7e733827dc1237f744a0b6db92c4fd66
Charles <98989744+Davidcharlie381@users.noreply.github.com>
2023-12-06 12:34:22 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -17,5 +17,6 @@ router.post('/files', FilesController.postUpload);
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
