8cf3872fced28bb967aa1dfc2b60b37316e16d90
Charles <98989744+Davidcharlie381@users.noreply.github.com>
2023-12-06 12:26:07 +0100

    Create FilesController.js

---
+++ controllers/FilesController.js
@@ -0,0 +1,87 @@
+import fs from 'fs';
+import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class FilesController {
+  static async postUpload(req, res) {
+    const token = req.header('X-Token');
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.getUserById(userId);
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const { name, type, parentId = 0, isPublic = false, data } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Missing name' });
+    }
+
+    if (!type || !['folder', 'file', 'image'].includes(type)) {
+      return res.status(400).json({ error: 'Missing type' });
+    }
+
+    if ((type === 'file' || type === 'image') && !data) {
+      return res.status(400).json({ error: 'Missing data' });
+    }
+
+    if (parentId !== 0) {
+      const parentFile = await dbClient.getFileById(parentId);
+
+      if (!parentFile) {
+        return res.status(400).json({ error: 'Parent not found' });
+      }
+
+      if (parentFile.type !== 'folder') {
+        return res.status(400).json({ error: 'Parent is not a folder' });
+      }
+    }
+
+    const storingFolderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+    const localPath = path.join(storingFolderPath, uuidv4());
+
+    if (type === 'file' || type === 'image') {
+      const fileDataBuffer = Buffer.from(data, 'base64');
+      fs.writeFileSync(localPath, fileDataBuffer);
+    } else if (type === 'folder') {
+      fs.mkdirSync(localPath);
+    }
+
+    const newFile = {
+      userId: user._id.toString(),
+      name,
+      type,
+      isPublic,
+      parentId,
+      localPath: type === 'folder' ? null : localPath,
+    };
+
+    const result = await dbClient.insertFile(newFile);
+
+    return res.status(201).json({
+      id: result.insertedId,
+      userId: newFile.userId,
+      name: newFile.name,
+      type: newFile.type,
+      isPublic: newFile.isPublic,
+      parentId: newFile.parentId,
+      localPath: newFile.localPath,
+    });
+  }
+}
+
+module.exports = FilesController;
